6f90a4c97bd21bd50249913a8ce9cd598a6910c6
Chris-Mwiti <chrismwiti21@gmail.com>
2026-04-27 02:15:01 +0300

    changes: Implemented a complete message parser(handles concurrent messaging) for the node

--- Messenger/message_parser/starter.go
+++ Messenger/message_parser/starter.go
@@ -29,7 +29,12 @@ func (n *Node) Send(dest string, body map[string]interface{}) {
 	n.mu.Unlock()
 
 	msg := Message{Src: n.NodeID, Dest: dest, Body: body}
-	output, _ := json.Marshal(msg)
+	output, err := json.Marshal(msg)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error while marshalling res: ", err)
+		return
+	}
 	
 	n.outMu.Lock()
 	fmt.Println(string(output))
@@ -43,10 +48,43 @@ func (n *Node) Reply(request Message, body map[string]interface{}) {
 	n.Send(request.Src, body)
 }
 
+func (n *Node) HandleMessages(msg Message) {
+
+	msgType, _ := msg.Body["type"].(string)
+	switch msgType {
+	case "init":
+
+		n.mu.Lock()
+		n.NodeID, _ = msg.Body["node_id"].(string)
+		if ids, ok := msg.Body["node_ids"].([]interface{}); ok {
+			for _, id := range ids {
+				n.NodeIDs = n.NodeIDs[:0]
+				n.NodeIDs = append(n.NodeIDs, id.(string))
+			}
+		}
+		n.mu.Unlock()
+		n.Reply(msg, map[string]interface{}{"type": "init_ok"})
+	case "echo":
+		// TODO: Handle echo message
+		// Reply with echo_ok containing the same echo value
+		body := make(map[string]interface{})
+
+		body["type"] = "echo_ok"
+		body["echo"] = msg.Body["echo"]
+		n.Reply(msg, body)
+	
+	default:
+		fmt.Println("Unknown command")
+	}
+
+
+}
+
 func main() {
 	node := &Node{}
 	scanner := bufio.NewScanner(os.Stdin)
 
+	var wg sync.WaitGroup
 	for scanner.Scan() {
 		var msg Message
 		if err := json.Unmarshal(scanner.Bytes(), &msg); err != nil {
@@ -54,24 +92,13 @@ func main() {
 			continue
 		}
 
-		msgType, _ := msg.Body["type"].(string)
-		switch msgType {
-		case "init":
-			node.NodeID, _ = msg.Body["node_id"].(string)
-			if ids, ok := msg.Body["node_ids"].([]interface{}); ok {
-				for _, id := range ids {
-					node.NodeIDs = append(node.NodeIDs, id.(string))
-				}
-			}
-			node.Reply(msg, map[string]interface{}{"type": "init_ok"})
-		case "echo":
-			// TODO: Handle echo message
-			// Reply with echo_ok containing the same echo value
-			body := make(map[string]interface{})
-
-			body["type"] = "echo_ok"
-			node.Reply(msg, body)
-		}
+		wg.Add(1)
+		go func(msg Message){
+			defer wg.Done()	
+			node.HandleMessages(msg)
+		}(msg)
+		wg.Wait()
 	}
+
 }
 
