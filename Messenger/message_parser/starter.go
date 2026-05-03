d705e496e03c00f9fcc08aca3db9a0b1f0079483
Chris-Mwiti <chrismwiti21@gmail.com>
2026-05-03 11:13:22 +0300

    changes: In the process of implementing a synchronous node serving rpc protocol

--- Messenger/message_parser/starter.go
+++ Messenger/message_parser/starter.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"encoding/json"
 	"fmt"
 	"os"
 	"sync"
+	"time"
 )
 
 type Node struct {
 	NodeID    string
 	NodeIDs   []string
 	NextMsgID int
+	ProxyRegistry map[string]interface{}
 	mu        sync.Mutex
 	outMu     sync.Mutex
 }
@@ -22,7 +25,7 @@ type Message struct {
 	Body map[string]interface{} `json:"body"`
 }
 
-func (n *Node) Send(dest string, body map[string]interface{}) {
+func (n *Node) Send(dest string, body map[string]interface{}) (*Message, error){
 	n.mu.Lock()
 	body["msg_id"] = n.NextMsgID
 	n.NextMsgID++
@@ -33,56 +36,134 @@ func (n *Node) Send(dest string, body map[string]interface{}) {
 
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error while marshalling res: ", err)
-		return
+		return nil, err
 	}
 	
 	n.outMu.Lock()
 	fmt.Println(string(output))
 	n.outMu.Unlock()
+
+	return &msg, nil
 }
 
-func (n *Node) Reply(request Message, body map[string]interface{}) {
+func (n *Node) Reply(request Message, body map[string]interface{})(*Message, error) {
 	if msgID, ok := request.Body["msg_id"].(float64); ok {
 		body["in_reply_to"] = int(msgID)
 	}
-	n.Send(request.Src, body)
+	res, err := n.Send(request.Src, body)
+	
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error while sending request", err)
+		return nil, err
+	}
+
+	return res, nil
 }
 
-func (n *Node) HandleMessages(msg Message) {
+func (n *Node) Init(msg Message) (*Message, error){
+	n.mu.Lock()
+	n.NodeID, _ = msg.Body["node_id"].(string)
+	if ids, ok := msg.Body["node_ids"].([]interface{}); ok {
+		for _, id := range ids {
+			n.NodeIDs = n.NodeIDs[:0]
+			n.NodeIDs = append(n.NodeIDs, id.(string))
+		}
+	}
+	n.mu.Unlock()
+	res, err := n.Reply(msg, map[string]interface{}{"type": "init_ok"})
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error while reply request: ", err)
+		return nil, err
+	}
+	return res, nil
+}
+
+func (n *Node) Echo(msg Message) (*Message, error){
+	// TODO: Handle echo message
+	// Reply with echo_ok containing the same echo value
+	body := make(map[string]interface{})
+
+	body["type"] = "echo_ok"
+	body["echo"] = msg.Body["echo"]
+
+	res, err := n.Reply(msg, map[string]interface{}{"type": "init_ok"})
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error while reply request: ", err)
+		return nil, err
+	}
+	return res, nil
+}
+
+func (n *Node) sync_rpc(msg Message, ctx context.Context, ch chan bool) (*Message, error){
+	innerReq := make(map[string]interface{})
+	innerReq = msg.Body["inner"].(map[string]interface{})
+	innerReq["msg_id"] = msg.Body["msg_id"]
+
+	//patched request
+	var reqMsg Message
+	reqMsg.Src = n.NodeID
+	//@todo: check if the target node for the proxy exists within the registry
+	reqMsg.Dest = msg.Body["target"].(string)	
+	reqMsg.Body = innerReq
+
+	return n.HandleMessages(reqMsg, ctx, ch)	
+}
+
+func (n *Node) HandleMessages(msg Message, ctx context.Context, ch chan bool) (*Message, error){
 
 	msgType, _ := msg.Body["type"].(string)
+	var globalRes *Message
+	var globalErr error
 	switch msgType {
 	case "init":
-
-		n.mu.Lock()
-		n.NodeID, _ = msg.Body["node_id"].(string)
-		if ids, ok := msg.Body["node_ids"].([]interface{}); ok {
-			for _, id := range ids {
-				n.NodeIDs = n.NodeIDs[:0]
-				n.NodeIDs = append(n.NodeIDs, id.(string))
-			}
+		res, err := n.Init(msg)
+		globalRes = res
+		globalErr = err
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "error while initiating node: ", err)
 		}
-		n.mu.Unlock()
-		n.Reply(msg, map[string]interface{}{"type": "init_ok"})
+		ch <- true
 	case "echo":
-		// TODO: Handle echo message
-		// Reply with echo_ok containing the same echo value
-		body := make(map[string]interface{})
+		res, err := n.Init(msg)
+		globalRes = res
+		globalErr = err
 
-		body["type"] = "echo_ok"
-		body["echo"] = msg.Body["echo"]
-		n.Reply(msg, body)
-	
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "error while initiating node: ", err)
+		}
+
+		ch <- true
+	case "proxy":
+		select{
+		case <- ctx.Done(): 
+			return nil, nil
+		default: 
+	    key := msg.Body["msg_id"].(string)	
+			res, err := n.sync_rpc(msg, ctx, ch)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "error while sync rpc: ", err)
+				ch <- true
+			}
+			n.ProxyRegistry[key] = *res
+			globalRes= res
+			globalErr = err
+			ch <- true
+		}
 	default:
 		fmt.Println("Unknown command")
 	}
 
+	return globalRes, globalErr
 
 }
 
 func main() {
 	node := &Node{}
 	scanner := bufio.NewScanner(os.Stdin)
+	ctx, cancel := context.WithTimeout(context.Background(), 60 * time.Second)
+	eventCh := make(chan bool)
+
+	defer cancel()
 
 	var wg sync.WaitGroup
 	for scanner.Scan() {
@@ -93,10 +174,11 @@ func main() {
 		}
 
 		wg.Add(1)
-		go func(msg Message){
+		go func(msg Message, ctx context.Context, ch chan bool){
 			defer wg.Done()	
-			node.HandleMessages(msg)
-		}(msg)
+			node.HandleMessages(msg, ctx, ch)
+			<-eventCh
+		}(msg, ctx, eventCh)
 		wg.Wait()
 	}
 
